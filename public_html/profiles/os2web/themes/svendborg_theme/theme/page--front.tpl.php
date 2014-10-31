87184fb085154e6d3a540548aa05449bf1fbe3c2
root <root@75157fd9a387>
2014-10-31 21:00:05 +0000

    Removed functions for frontpage variables to template.php

--- public_html/profiles/os2web/themes/svendborg_theme/theme/page--front.tpl.php
+++ public_html/profiles/os2web/themes/svendborg_theme/theme/page--front.tpl.php
@@ -11,7 +11,7 @@
       <!-- page--front.tpl.php-->
       <div class="row-no-padding front-buttons col-md-push-1 col-md-10 col-sm-12 col-xs-12">
         <?php module_load_include('module', 'svendborg_views', 'svendborg_views.module'); ?>
-        <?php svendborg_views_get_front_big_menu_buttons(); ?>
+        <?php print $page['front_big_menu']; ?>
       </div>
 
       <div class="front-search-box col-md-push-3 col-md-6 col-sm-push-3 col-sm-6 col-xs-12">
@@ -21,7 +21,7 @@
 
       <div class="col-md-push-1 col-md-10 col-xs-12">
         <div id="front-news-branding" class="front-news-branding col-md-12 col-sm-12 col-xs-12 carousel slide" data-ride="carousel">
-          <?php svendborg_views_get_front_large_carousel(); ?>
+          <?php print $page['front_large_carousel']; ?>
         </div>
       </div>
     </div>
@@ -34,7 +34,7 @@
 <div class="lcontainer-fluid clearfix front-s-news">
   <div class="container front-s-news-inner">
     <div class="row">
-      <?php svendborg_views_get_front_small_carousel(); ?>
+      <?php print $page['front_small_carousel']; ?>
     </div>
   </div>
 </div>
