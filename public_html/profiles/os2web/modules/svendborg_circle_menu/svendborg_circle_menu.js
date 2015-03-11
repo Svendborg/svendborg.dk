4e20d93e60e3af76358ef71364c30b98103991ef
Yani0029 <yani@bellcom.dk>
2015-03-11 09:36:59 +0100

    Fixed link title text bug

--- public_html/profiles/os2web/modules/svendborg_circle_menu/svendborg_circle_menu.js
+++ public_html/profiles/os2web/modules/svendborg_circle_menu/svendborg_circle_menu.js
@@ -2,11 +2,11 @@
   $(document).ready(function() {
     $("body").append("<div id='ToolTipDiv'></div>");
     $(".front-indholsdmenu a").each(function() {
+      var offset = $(this).offset();
       $(this).hover(function(e) {
         $(this).mousemove(function(e) {
-          var tipY = e.pageY;
-          var tipX = e.pageX;
-          $("#ToolTipDiv").css({'top': tipY, 'left': tipX});
+
+          $("#ToolTipDiv").css({'top': offset.top + 30, 'left': offset.left + 30});
         });
         $("#ToolTipDiv").stop(true, true);
         $("#ToolTipDiv")
@@ -15,7 +15,6 @@
         $(this).removeAttr('title');
       }, function() {
         $("#ToolTipDiv").stop(true, true);
-        console.log(Drupal.settings.text_duration_time);
         $("#ToolTipDiv").fadeOut(Drupal.settings.text_duration_time * 1000);
         $(this).attr('title', $("#ToolTipDiv").html());
       });
