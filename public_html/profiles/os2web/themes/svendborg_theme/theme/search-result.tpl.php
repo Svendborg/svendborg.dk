92fd00e8c7d56df41aaa195b65c39b282dced27a
Franz Skaaning <fs@bellcom.dk>
2015-07-08 10:54:23 +0200

    bg info

--- public_html/profiles/os2web/themes/svendborg_theme/theme/search-result.tpl.php
+++ public_html/profiles/os2web/themes/svendborg_theme/theme/search-result.tpl.php
@@ -72,7 +72,7 @@
   <?php print render($title_suffix); ?>
   <div class="search-snippet-info">
     <?php if ($snippet): ?>
-      <p class="search-snippet"<?php print $content_attributes; ?>><pre><?php print $snippet; ?></pre></p>
+      <p class="search-snippet bg-info"<?php print $content_attributes; ?>><pre><?php print $snippet; ?></pre></p>
     <?php endif; ?>
   </div>
 </li>
