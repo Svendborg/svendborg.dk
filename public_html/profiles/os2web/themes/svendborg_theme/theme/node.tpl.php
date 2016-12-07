a148f47abbda18e8a423e5f937a2353ef73fb75b
Stanislav Kutasevits <stan@bellcom.dk>
2016-12-07 15:19:47 +0200

    adding borger.dk mirgate files

--- public_html/profiles/os2web/themes/svendborg_theme/theme/node.tpl.php
+++ public_html/profiles/os2web/themes/svendborg_theme/theme/node.tpl.php
@@ -111,7 +111,7 @@
     <?php else : ?>
       <?php print render($content['field_os2web_base_field_image']); ?>
     <?php endif; ?>
-    <?php if ($node->type != 'os2web_base_contentpage' &&  $node->type != 'os2web_borger_dk_article'): ?>
+    <?php if ($node->type != 'os2web_base_contentpage' &&  $node->type != 'os2web_borger_dk_article' && $node->type != 'borger_dk_article'): ?>
     <time pubdate="pubdate">
       <i></i><?php print format_date($created, 'custom', 'j. F Y'); ?>
     </time>
